952acba8bbc603e52ec05ffb8dadfe655319ef5c
Mawuse <akpekemawuse1@gmail.com>
2026-08-16 20:03:50 +0000

    push

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Navbar from "./Navbar";
 import Footer from "../pages/Footer";
 
 const RootLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <div className="min-h-screen overflow-x-clip bg-[#f5f1e9]">
       <Navbar />
